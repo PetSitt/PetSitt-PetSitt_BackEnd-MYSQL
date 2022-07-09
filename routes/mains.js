d979c8291fd2b844233e831e80f5ff9891c8501d
jeong-hyeonkim <jeong-hyeonkim>
2022-07-09 22:38:13 +0900

    first-commit

--- routes/mains.js
+++ routes/mains.js
@@ -4,7 +4,7 @@ const {Op} = require("sequelize")
 const { Sitter,sequelize} = require("../models");
 
 router.post("/", async (req, res) => {
-<<<<<<< HEAD
+
   const {x,y} = req.body;
   const sitters = await Sitter.findAll({
     location :{
@@ -13,73 +13,18 @@ router.post("/", async (req, res) => {
       }
 })
     res.send({sitters})
-=======
-  const {x,y, walk, wash, prac, dayCare, boarding} = req.body;
-  const sitters = await Sitter.find({
-    location: {
-      $nearSphere: {
-        $geometry: {
-          type: "Point",
-          coordinates: [x, y],
-        },
-        $maxDistance: 10000000,
-      },
-    },
-    category:{
-      $nin: [
-        walk, wash, prac, dayCare, boarding,
-      ]}
-    ,
-  })
-  res.send({
-    sitters,
->>>>>>> 4ceab9f933bf4e4a813de60a4d2cbbdb526bbffa
+
   });
 
 
 router.post("/search", async (req, res) => {
-<<<<<<< HEAD
+
   const {region_2depth_name,searchDate, walk, wash, prac, dayCare, boarding } = req.body;
   const sitters = await Sitter.findAll({
     attributes:[region_2depth_name,searchDate,walk, wash, prac, dayCare, boarding],
         region_2depth_name:{
         eq:region_2depth_name,
-=======
-  const {region_2depth_name, x, y,searchDate, walk, wash, prac, dayCare, boarding } = req.body;
-  const sitters = await Sitter.find({
-    region_2depth_name:{
-      $eq:region_2depth_name,
-    },
-    noDate:{
-      $nin:
-        searchDate,
-      },
-    category:{
-      $nin: [
-        walk, wash, prac, dayCare, boarding,
-      ]}
-});
-if(!sitters?.length){
-  const sitter2 = await Sitter.find({$and:[
-    {noDate:{
-      $nin:
-        searchDate,
-      }
-    },
-    {category:{
-      $nin: [
-        walk, wash, prac, dayCare, boarding,
-      ]}
-    },
-],
-    location: {
-      $nearSphere: {
-        $geometry: {
-          type: "Point",
-          coordinates: [x, y],
-        },
-        $maxDistance: 10000000,
->>>>>>> 4ceab9f933bf4e4a813de60a4d2cbbdb526bbffa
+
       },
         noDate:{
           [Op.notIn]:[searchDate
