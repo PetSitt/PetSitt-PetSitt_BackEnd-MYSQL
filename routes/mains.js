06dc9db41d81a119348f4f9b35aca36ebd2562da
jeong-hyeonkim <jeong-hyeonkim>
2022-08-02 17:51:09 +0900

    메인 무한스크롤 추가

--- routes/mains.js
+++ routes/mains.js
@@ -4,36 +4,49 @@ const { Sitter } = require("../models");
 const { Op, Sequelize } = require("sequelize");
 
 router.post("/", async (req, res) => {
-  const { x, y, category } = req.body;
-  if (x === "undefined" || y === "undefined" || !x || !y) {
-    x = 126.875078748377;
-    y = 37.4856025065543;
-  }
-  const location = Sequelize.literal(`ST_GeomFromText('POINT(${x} ${y})')`);
-  const distance = Sequelize.fn(
-    "ST_Distance",
-    Sequelize.col("location"),
-    location
-  );
-  
-  const sitter = [];
-  const sitters = await Sitter.findAll({
-    order: distance,
-    where: Sequelize.where(distance, { [Op.lte]: 0.5 }),
-  });
-  for (i = 0; i < sitters.length; i++) {
-    const intersection2 = category.filter((x) =>
-      sitters[i].category.includes(x)
+  try {
+    const { limit, offset, x, y, category } = req.body;
+
+    const location = Sequelize.literal(`ST_GeomFromText('POINT(${x} ${y})')`);
+    const distance = Sequelize.fn(
+      "ST_Distance",
+      Sequelize.col("location"),
+      location
     );
-    if (intersection2.length === category.length) {
-      sitter.push(sitters[i]);
+    if (x === "undefined" || y === "undefined" || !x || !y) {
+      x = 126.875078748377;
+      y = 37.4856025065543;
     }
+
+    const sitter = [];
+    const sitters = await Sitter.findAll({
+      offset: offset,
+      limit: limit,
+      order: distance,
+      where: Sequelize.where(distance, { [Op.lte]: 0.5 }),
+    });
+
+    for (i = 0; i < sitters.length; i++) {
+      const intersection2 = category.filter((x) =>
+        sitters[i].category.includes(x)
+      );
+      if (intersection2.length === category.length) {
+        sitter.push(sitters[i]);
+      }
+    }
+
+    if (!sitter.length) {
+      return res.status(200).send({ sitter: false });
+    }
+
+    return res.status(200).send({ sitter });
+  } catch {
+    res.status(400).send({ errorMessage: "시터 정보가 없습니다." });
   }
-  res.send({ sitter });
 });
 
 router.post("/search", async (req, res) => {
-  const { region_2depth_name, searchDate, category, x, y } = req.body;
+  const { region_2depth_name, searchDate, category, x, y, limit, offset } = req.body;
   const location = Sequelize.literal(`ST_GeomFromText('POINT(${x} ${y})')`);
   const distance = Sequelize.fn(
     "ST_Distance",
@@ -43,6 +56,8 @@ router.post("/search", async (req, res) => {
 
   const sitters = [];
   const sitters2 = await Sitter.findAll({
+    offset: offset,
+    limit: limit,
     where: {
       region_2depth_name: { [Op.eq]: region_2depth_name },
     },
@@ -68,6 +83,8 @@ router.post("/search", async (req, res) => {
 
   if (!sitters?.length) {
     const sitters2 = await Sitter.findAll({
+      offset: offset,
+      limit: limit,
       order: distance,
       where: Sequelize.where(distance, { [Op.lte]: 0.5 }),
     });
